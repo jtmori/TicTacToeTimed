b4ffc219f519f49dab46aa849b615dda5aec9157
Jenn <jmori1207@gmail.com>
2016-10-02 20:59:51 -0400

    Play-Game Layout created + connecting the two activities

--- app/src/main/java/com/miami/c11305010/timedtictactoe/MainActivity.java
+++ app/src/main/java/com/miami/c11305010/timedtictactoe/MainActivity.java
@@ -1,13 +1,28 @@
 package com.miami.c11305010.timedtictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    protected final int PLAY_GAME_RETURN_OKAY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickHandler(View view){
+        int id = view.getId();
+        switch (id){
+            case R.id.start_button:
+                Intent intent = new Intent(MainActivity.this, PlayGameActivity.class);
+                startActivityForResult(intent, PLAY_GAME_RETURN_OKAY);
+                break;
+            default:
+                break;
+        }
     }
 }
