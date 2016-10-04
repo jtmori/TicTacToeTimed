70499cda41e67977751b10f947fcf3e39218d0dd
Jenn <jmori1207@gmail.com>
2016-10-04 16:30:53 -0400

    onActivityResult from tictactoe created

--- app/src/main/java/com/miami/c11305010/timedtictactoe/MainActivity.java
+++ app/src/main/java/com/miami/c11305010/timedtictactoe/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     protected final int PLAY_GAME_RETURN_OKAY = 1;
@@ -20,6 +22,37 @@ public class MainActivity extends AppCompatActivity {
             case R.id.start_button:
                 Intent intent = new Intent(MainActivity.this, PlayGameActivity.class);
                 startActivityForResult(intent, PLAY_GAME_RETURN_OKAY);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        int increment = 1;
+        RatingBar p1_ratingBar = (RatingBar)findViewById(R.id.player1_rating);
+        RatingBar p2_ratingBar = (RatingBar)findViewById(R.id.player2_rating);
+
+        switch (requestCode){
+            case PLAY_GAME_RETURN_OKAY:
+                if (resultCode == RESULT_OK){
+                    int winner = data.getExtras().getInt(getResources().getString(R.string.winnerKey));
+                    if (winner == 1){  //data = 1 -> player 1 won
+                        p1_ratingBar.setRating(p1_ratingBar.getRating()+increment);
+                        final int rating = (int)p1_ratingBar.getRating();
+                        if(rating == (getResources().getInteger(R.integer.max_score))){
+                            Toast.makeText(MainActivity.this, "P1 WINS!", Toast.LENGTH_LONG).show();
+                        }
+                    } else if (winner == 2){ //data = 2 -> player 2 won
+                        p2_ratingBar.setRating(p2_ratingBar.getRating()+increment);
+                        final int rating = (int)p2_ratingBar.getRating();
+                        if(rating == (getResources().getInteger(R.integer.max_score))){
+                            Toast.makeText(MainActivity.this, "P2 WINS!", Toast.LENGTH_LONG).show();
+                        }
+                    } else
+                        Toast.makeText(MainActivity.this, "NO WINNER", Toast.LENGTH_LONG).show();
+                }
                 break;
             default:
                 break;
--- app/src/main/java/com/miami/c11305010/timedtictactoe/PlayGameActivity.java
+++ app/src/main/java/com/miami/c11305010/timedtictactoe/PlayGameActivity.java
@@ -1,13 +1,55 @@
 package com.miami.c11305010.timedtictactoe;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 public class PlayGameActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    private int barClickTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+
+        progressBar = (ProgressBar)findViewById(R.id.progress);
+        progressBar.setProgress(0);
+        barClickTime = getResources().getInteger(R.integer.bar_click_time);
+        myProgresser.run();
+    }
+    //--------------------------------------------------------------------
+    private final Runnable myProgresser = new Runnable() {
+        private Handler myHandler = new Handler();
+
+        public void run() {
+            Log.i("IN runnable", "Inside OK");
+            progressBar.setProgress(progressBar.getProgress()+ barClickTime);
+            if (progressBar.getProgress() >= getResources().getInteger(R.integer.bar_time)) {
+                Log.i("IN runnable", "Progress bar full");
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                progressBar.setProgress(0);
+                finish();
+            }
+            if (!myHandler.postDelayed(myProgresser,barClickTime)) {
+                Log.e("ERROR","Cannot postDelayed");
+            }
+        }
+
+    };
+
+    //CODE FOR THE TICTACTOE PART
+
+    //--------------------------------------------------------------------------
+    @Override
+    public void onBackPressed() {
+        Log.i("IN backpressed", "back button pressed");
+        Intent gobackIntent = new Intent();
+        setResult(RESULT_CANCELED, gobackIntent);
+        finish();
     }
 }
